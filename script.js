5b4e2c0b15ee70b9b396a4f2144249b58a9afc07
lovatto89 <fernandolovatto.mariani@gmail.com>
2025-06-16 15:59:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let botao1 = document.querySelector("#botao1");
+botao1.addEventListener("click", e =>{
+        botao1.style.background="lightblue"
+        botao1.innerHTML="Um sonho é apenas um desejo, até o momento em que você começa a atuar sobre ele, e propõe-se a transformá-lo em uma meta."
+});
